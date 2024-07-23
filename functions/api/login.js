63b54372153f04564d72a97d32c5b58806e58bb3
scomurr <scomurr@hotmail.com>
2024-07-23 10:42:57 -0500

    login api

--- functions/api/login.js
+++ functions/api/login.js
@@ -1,18 +1,19 @@
-export async function onRequestPost(request) {
+export async function onRequestPost(context) {
+    const { request } = context;
     // Check for POST request to "/api/login"
     if (request.method !== 'POST' || !request.url.endsWith('/api/login')) {
-        return new Response('BB Not Found:' + request.method + ": " + request.url, { status: 404 });
-        }
-    
-        // Get username and password from request body
-        const formData = await request.formData();
-        const username = formData.get('username');
-        const password = formData.get('password');
-    
-        // Check credentials
-        if (username === 'admin' && password === 'admin') {
-        return new Response('Login Successful', { status: 200 });
-        } else {
-        return new Response('Wrong Username or Password', { status: 401 });
-        }
+        return new Response('CC Not Found:' + request.method + ": " + request.url, { status: 404 });
+    }
+
+    // Get username and password from request body
+    const formData = await request.formData();
+    const username = formData.get('username');
+    const password = formData.get('password');
+
+    // Check credentials
+    if (username === 'admin' && password === 'admin') {
+    return new Response('Login Successful', { status: 200 });
+    } else {
+    return new Response('Wrong Username or Password', { status: 401 });
+    }
 }
